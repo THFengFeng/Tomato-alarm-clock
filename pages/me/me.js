ff39c77ab0a407607f382d93485c5bd660628edd
tonghaifeng <632930757@qq.com>
2020-08-25 21:10:25 +0800

    完成登录 绑定页面

--- pages/me/me.js
+++ pages/me/me.js
@@ -1,25 +1,18 @@
 // pages/me/me.js
 Page({
-
-    /**
-     * 页面的初始数据
-     */
     data: {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
+        tab: "tomato",
+        lists: {
+            "本周四": [
+                {time: "14:00", text: "阿含经开发你看看书法课", id: 1},
+                {time: "14:00", text: "阿含经开发你看看书法课", id: 4}
+            ],
+            "本周五": [{time: "14:00", text: "阿含经开发你看看书法课", id: 2}],
+            "本周天": [{time: "14:00", text: "阿含经开发你看看书法课", id: 3}],
+            "本周一": [{time: "14:00", text: "阿含经开发你看看书法课", id: 3}],
+            "本周二": [{time: "14:00", text: "阿含经开发你看看书法课", id: 3}],
+            "本周三": [{time: "14:00", text: "阿含经开发你看看书法课", id: 3}],
+        }
     },
 
     /**
@@ -29,38 +22,8 @@ Page({
 
     },
 
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+    changeTab(event) {
+        let name = event.currentTarget.dataset.name
+        this.setData({tab: name})
     }
 })
